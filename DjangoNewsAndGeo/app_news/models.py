d17f3857b6f361477dccbc7a8e3a43117fa0b273
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2023-08-25 15:53:21 +0700

    удаление всего лишнего из index и создание модели News

--- DjangoNewsAndGeo/app_news/models.py
+++ DjangoNewsAndGeo/app_news/models.py
@@ -3,5 +3,22 @@ Definition of models.
 """
 
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class News(models.Model):
+    """Модель новости"""
+    title = models.CharField(max_length=200, verbose_name=_('Title'))
+    description = models.TextField(verbose_name=_('Description'))
+    image = models.ImageField(upload_to='images/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('News')
+        verbose_name_plural = _('News')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('news_detail', args=[str(self.pk)])
